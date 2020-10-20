10b3fa016cefa5b88d6dbc2054bff3bbf91d28a7
TJFitz <tjfitz@comcast.net>
2020-10-19 21:38:22 -0400

    repull

--- src/components/Fullpage/Fullpage.js
+++ src/components/Fullpage/Fullpage.js
@@ -2,5 +2,5 @@ import React from "react";
 import "./style.css";
 
 export default function Fullpage(props) {
-  return <div className="bg-dark fullpageWidth">{props.children}</div>;
+  return <div className="bg-dark fullpageWidth vh-100">{props.children}</div>;
 }
