41220b24aca1fa8f33b613e7763ee42f2212f7eb
TJFitz <tjfitz@comcast.net>
2020-10-27 17:29:54 -0400

    Updates

---
+++ precache-manifest.798c61ea88186374fc2548d843be1b46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2cf68f4008649e7b724f8d50886b2f3b",
+    "url": "/React_Employee_Directory/index.html"
+  },
+  {
+    "revision": "896a5c9810380d9a7047",
+    "url": "/React_Employee_Directory/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e4dfc6cf0fbad935a17c",
+    "url": "/React_Employee_Directory/static/css/main.72fd4913.chunk.css"
+  },
+  {
+    "revision": "896a5c9810380d9a7047",
+    "url": "/React_Employee_Directory/static/js/2.ddee5a58.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/React_Employee_Directory/static/js/2.ddee5a58.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4dfc6cf0fbad935a17c",
+    "url": "/React_Employee_Directory/static/js/main.7cd13745.chunk.js"
+  },
+  {
+    "revision": "d72aea3905fc1330a49d",
+    "url": "/React_Employee_Directory/static/js/runtime-main.e01549dd.js"
+  }
+]);
